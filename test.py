ce4b3404690f50479c34bfb72988a4099c89e0f3
MikeTheReader <mlundin617@gmail.com>
2017-03-21 11:57:42 -0700

    Some hotfix changes

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ def some_new_function():
     print('some_new_function --masterVersion')
 
 def some_other_new_function():
-    print('some_other_new_function, again')
+    print('some_other_new_function, again -- hotfixVersion')

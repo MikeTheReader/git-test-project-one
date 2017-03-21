f7be09e45bf620b0daf2bfd07009d6ccfb03a820
MikeTheReader <mlundin617@gmail.com>
2017-03-21 11:54:01 -0700

    made master changes

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 def some_new_function():
-    print('some_new_function')
+    print('some_new_function --masterVersion')
 
 def some_other_new_function():
     print('some_other_new_function, again')

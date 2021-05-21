2f6c3f5e882070840cb0a74e610938afc9dfe85d
Michael Lundin <mikel@ifit.com>
2021-05-21 13:47:34 -0700

    fix: this is the first commit I made

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ def new_add_function():
 def some_other_new_function():
     print('some_other_new_function, again -- hotfixVersion')
 
+def first_commit_function():
+    print('some function created during the first commit');
+
 def second_branch():
     print('second branch')
 

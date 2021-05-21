38b009c647af3f7764b2d6cad643a7eba88d243d
Mike Lundin <MikeTheReader@users.noreply.github.com>
2021-05-21 13:53:21 -0700

    Squash Commits Pull Request (#6)

--- test.py
+++ test.py
@@ -7,6 +7,12 @@ def new_add_function():
 def some_other_new_function():
     print('some_other_new_function, again -- hotfixVersion')
 
+def first_commit_function():
+    print('some function created during the first commit');
+
+def second_commit_function():
+    print('some function created during the second commit');
+
 def second_branch():
     print('second branch')
 

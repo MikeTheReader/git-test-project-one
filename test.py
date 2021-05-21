9dfd3d6fcd0bf117d09087fe386d77e7d9d3b46f
Michael Lundin <mikel@ifit.com>
2021-05-21 13:48:12 -0700

    feat: added whole new feature
    BREAKING CHANGE: This has a breaking change in it.

--- test.py
+++ test.py
@@ -10,6 +10,9 @@ def some_other_new_function():
 def first_commit_function():
     print('some function created during the first commit');
 
+def second_commit_function():
+    print('some function created during the second commit');
+
 def second_branch():
     print('second branch')
 
